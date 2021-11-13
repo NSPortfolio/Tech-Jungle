e044098fb1e7e9a09adb9db990680d93747b833f
Times4Everyone <76495035+Times4Everyone@users.noreply.github.com>
2021-11-12 21:04:25 -0600

    Update settings.py

--- techjungle/settings.py
+++ techjungle/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'techjungle.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
